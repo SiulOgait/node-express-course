45f336a8ec0f836078b1af2e890031c36252b5e5
SiulOgait <luistiagoc.silva@googlemail.com>
2020-10-22 21:39:18 +0100

    add dynamic GET route using variables. TT

--- server.js
+++ server.js
@@ -9,16 +9,30 @@ const mockUserData = [
     { name: 'Tiago' }, { name: 'William' }
 ];
 
+
+//This function will respond to a GET request at http://localhost:8000/users with a JSON file, which includes our mockData under the key 'users'.
 app.get('/users', function(req, res) {
     res.json({
         success: true,
-        message: 'successfully got users. Nice!',
+        message: 'We successfully got your users. Very Nice Tiagolas!',
         users: mockUserData
     })
 })
 
+
+//In Express, words with a colon in front of them in the url are treated as variables. You can access the value of each variable through req.params, like this:
+app.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'Tiagolas we got one user. Check the Git Bash terminal!',
+        users: mockUserData
+    })
+
+})
+
 app.listen(8000, function() {
-    console.log("My first Node.js Server is running")
+    console.log("My first Tiagolas Node.js Server is running")
 })
 
 /* The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
